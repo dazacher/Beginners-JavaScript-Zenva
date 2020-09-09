380a6ff1f3dcd3d3a1508ca526cf87cc8d22c97f
dazacher <dazacher2000@yahoo.com>
2020-09-08 19:04:41 -0700

    If statements, concurrent if statements and nested if statements.

--- oldJavaScriptCode.js
+++ oldJavaScriptCode.js
@@ -132,3 +132,95 @@ firstLevel = [0][1] = 1.4;//changes 1.2 to 1.4
 
 ver secondWorld = levels[1].pop()
 var thridWorld = levels[2].push(3.3)
+
+function(parameters) {
+    function body
+    return outputs
+}
+
+
+var currentHealth = 50;
+var healAmount = 10;
+
+function heal() {
+    currentHealth += healAmount;
+    function nested() {
+        //does not exist outside the heal function
+    }
+}
+
+heal(); //curentHealth = 60
+
+// func is a refrence to the entire functions
+var func = function heal() {
+    currentHealth += healAmount;
+}
+
+
+
+//parameters are only inputs to a function
+var maxHealth = 100;
+var currentHealth = 50;
+//var healAmount = 10;
+
+function heal(healAmount) {
+    currentHealth += healAmount;
+}
+
+heal(10); // changes currentHealth to 60 by passing the value into the function without having to declare a variable
+
+
+//Functions
+function heal(healAmount = 10) {
+    var newHealth = currentHealth + healAmount;
+    currentHealth = newHealth > 100 ? maxHealth : newHealth;
+    return calculatePercent(currentHealth, maxHealth);
+}
+
+function calculatePercent(dividend, divisor) {
+    (dividend / divisor) * 100; //fuction immediately exits at this point and ignores any code after it
+}
+
+var result = heal(); //changes currentHealth to 60
+heal(50); //changes currentHealth to 100 and result = 100
+
+// if statements
+
+keyPress = "l";
+var xPos = 0;
+
+if (keyPress === "l") {
+    xPos -= 1; // xPos = -1
+} else if (keyPress === "r") {
+    xPos += 1;
+} else {
+    xPos = 0;
+}
+
+// Multiple concurrent if statements
+var someNum = 5
+if (someNum > 4) {
+    xPos -= 1; // xPos = -1
+}
+
+if (someNum > 2) {
+    xPos += 1;
+}
+
+//Nested if statements
+keyPress = "l";
+var xPos = 0;
+let leftEndPos = 5;
+let rightEndPos = -5;
+
+if (keyPress === "l") {
+    if (xPos < leftEndPos) {
+        xPos -= 1; // xPos = -1
+    }
+} else if (keyPress === "r") {
+    if (xPos > rightEndPos) {
+        xPos += 1;
+    }
+} else {
+    xPos = 0;
+}
