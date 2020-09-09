d6119fde89179d7f7153ea6e353bb0278566b4ea
dazacher <dazacher2000@yahoo.com>
2020-09-08 19:25:50 -0700

    Break, continue, return in While loops.

--- javaScriptCode.js
+++ javaScriptCode.js
@@ -14,9 +14,47 @@ var isGameOver = false;
 
 while (!isGameOver) {
     pos++;
-    if ( pos >= endPos || pos === enemyPos) {
+    if (pos >= endPos || pos === enemyPos) {
         isGameOver = true;
     }
 }
 
 //Control Loop Statements
+// break exit out of loops prematurely
+while (pos < endPos) {
+    pos++;
+    if (pos === enemyPos) {
+        break;
+    }
+}
+
+//continue lets you skip a loop iteration
+while (pos < endPos) {
+    if (pos % 2 === 1) {
+        pos += 2;
+        continue;
+    }
+    pos++;
+    if (pos === enemyPos) {
+        break;
+    }
+}
+
+//return statement
+function doSomething(someNum) {
+    var a = 5;
+    var result = a + someNum;
+    return;
+    a += 10; //This never gets executed
+}
+
+function movePlayer() {
+    while (pos < endPos) {
+        pos++;
+        if (pos === enemyPos) {
+            return;// exits out of the function
+        }
+    }
+}
+
+//For loops
